592ebc81bd98602563b1321e2dbbf78ce024228f
Filip <filip.bergander1996@gmail.com>
2026-01-30 13:33:37 +0100

    Lagt till två bilder, samt valt kvaliteter genom vite-image-optimizer.

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,14 @@
 "use strict";
 import '/src/sass/main.scss';
+
+const darkBtn = document.querySelector(".btn-darkened"); // Knapp för att toggla mörkt/ljust tema på startsidan.
+
+darkBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark"); // Lägger till classlist dark på body.
+    darkBtn.classList.toggle("dark"); // Lägger till classlist dark på knappen
+    if (bodyDark) {
+        darkBtn.innerHTML = "Ljust tema"; // Ändrar texten inom knappen
+    } else {
+        darkBtn.innerHTML = "Mörkt tema";
+    }
+})
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
     build: {
@@ -12,9 +13,12 @@ export default defineConfig({
                 map: resolve(__dirname, "map.html")
             }
         }
-    }
-
+    },
     plugins: [
-        {}
+        ViteImageOptimizer({
+            jpg: {
+                quality: 60
+            }
+        })
     ]
 });
