fb5cfa94e2ebe9e694f31169b0647277abb316f5
Filip <filip.bergander1996@gmail.com>
2026-02-04 13:00:00 +0100

    Lagt  till hamburgermeny för skärmar mindre än 475px.

--- src/js/main.js
+++ src/js/main.js
@@ -2,26 +2,30 @@
 import '/src/sass/main.scss';
 
 const toggleBtn = document.getElementById("btn-menu");
-const closeBtn = document.getElementById("btn-close")
+const closeBtn = document.getElementById("btn-close");
+const menuArea = document.getElementById("nav-menu");
 
 const darkBtn = document.querySelector(".btn-darkened"); // Knapp för att toggla mörkt/ljust tema på startsidan.
 
-darkBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark"); // Lägger till classlist dark på body.
-    darkBtn.classList.toggle("dark"); // Lägger till classlist dark på knappen
-    if (document.body.classList.contains("dark")) { // Om body.dark finns ändras knappens text
-        darkBtn.innerHTML = "Ljust tema"; // Ändrar texten inom knappen
-    } else {
-        darkBtn.innerHTML = "Mörkt tema";
-    }
-})
-
+if (darkBtn) {
+    darkBtn.addEventListener("click", () => {
+        document.body.classList.toggle("dark"); // Lägger till classlist dark på body.
+        darkBtn.classList.toggle("dark"); // Lägger till classlist dark på knappen
+        if (document.body.classList.contains("dark")) { // Om body.dark finns ändras knappens text
+            darkBtn.innerHTML = "Ljust tema"; // Ändrar texten inom knappen
+        } else {
+            darkBtn.innerHTML = "Mörkt tema";
+        }
+    })
+}
 toggleBtn.addEventListener("click", () => {
     toggleBtn.classList.toggle("close");
     closeBtn.classList.toggle("show");
+    menuArea.classList.toggle("show");
 });
 
 closeBtn.addEventListener("click", () => {
     closeBtn.classList.remove("show");
     toggleBtn.classList.remove("close");
+    menuArea.classList.remove("show");
 });
