bd7b8f8b9c5a592966b1225b0e75a8ed92124a33
Filip <filip.bergander1996@gmail.com>
2026-02-09 12:33:02 +0100

    Kommentarer samt ändrat färg inom menyn i mobilläge, när man togglar mörkt/ljust-läge genom knappen på startsidan.

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 "use strict";
 import '/src/sass/main.scss';
 
-const toggleBtn = document.getElementById("btn-menu");
-const closeBtn = document.getElementById("btn-close");
-const menuArea = document.getElementById("nav-menu");
+const toggleBtn = document.getElementById("btn-menu"); // Hamburgermeny
+const closeBtn = document.getElementById("btn-close"); // Stänga Hamburgermeny
+const menuArea = document.getElementById("nav-menu"); // Navigeringsmenyn
 
 const darkBtn = document.querySelector(".btn-darkened"); // Knapp för att toggla mörkt/ljust tema på startsidan.
 
@@ -13,19 +13,23 @@ if (darkBtn) {
         darkBtn.classList.toggle("dark"); // Lägger till classlist dark på knappen
         if (document.body.classList.contains("dark")) { // Om body.dark finns ändras knappens text
             darkBtn.innerHTML = "Ljust tema"; // Ändrar texten inom knappen
-            toggleBtn.classList.add("dark");
-        } else {
+            toggleBtn.classList.add("dark"); // Lägger till klassen mörk
+            closeBtn.classList.add("dark");
+            menuArea.classList.add("dark");
+        } else { // Om användaren klickar på knappen för att toggla ljust tema
             darkBtn.innerHTML = "Mörkt tema";
-            toggleBtn.classList.remove("dark");
+            toggleBtn.classList.remove("dark"); // Tar bort klassen mörk
+            closeBtn.classList.remove("dark");
+            menuArea.classList.remove("dark");
         }
     })
-}
+} // För att öppna hamburgermeny
 toggleBtn.addEventListener("click", () => {
     toggleBtn.classList.toggle("close");
     closeBtn.classList.toggle("show");
     menuArea.classList.toggle("show");
 });
-
+// För att stänga hamburgermeny
 closeBtn.addEventListener("click", () => {
     closeBtn.classList.remove("show");
     toggleBtn.classList.remove("close");
